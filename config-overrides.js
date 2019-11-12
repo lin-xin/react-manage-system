75dadfa63f43e85925858cb2b394c0f27a247d6c
linxin <linxin@oaserver.dw.gdbbk.com>
2019-11-12 17:49:22 +0800

    config:关闭sourcemap

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,11 @@
 const { override, fixBabelImports } = require('customize-cra');
 
+process.env.GENERATE_SOURCEMAP = 'false'; // 关闭sourcemap
+
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: 'css',
-  }),
+	fixBabelImports('import', {
+		libraryName: 'antd',
+		libraryDirectory: 'es',
+		style: 'css'
+	})
 );
