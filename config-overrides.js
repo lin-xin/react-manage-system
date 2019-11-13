a638e440a4a84a7f79b7841b5094d2c31c6927af
linxin <linxin@oaserver.dw.gdbbk.com>
2019-11-13 11:49:04 +0800

    :wrench:新增切换主题色配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const { override, fixBabelImports } = require('customize-cra');
+// const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 process.env.GENERATE_SOURCEMAP = 'false'; // 关闭sourcemap
 
@@ -7,5 +8,14 @@ module.exports = override(
 		libraryName: 'antd',
 		libraryDirectory: 'es',
 		style: 'css'
+		// style: true
 	})
+	// 修改主题色
+	// addLessLoader({
+	// 	javascriptEnabled: true,
+	// 	modifyVars: {
+	// 		'@primary-color': '#ffaa22',
+	// 		'@link-color': '#ffaa22'
+	// 	}
+	// })
 );
